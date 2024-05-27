54e3d8fac22eb6c849c33a4eb0e7276f9e630333
Ecordovaj <168248461+Ecordovaj@users.noreply.github.com>
2024-05-26 20:56:40 -0600

    .
    .

---
+++ segundo trabajo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main ()
+
+{
+int n1,n2,multi;
+
+std :: cout << "inserte su primer numero";
+std :: cin >> n1;
+
+std :: cout << "inserte el segundo numero";
+std :: cin >> n2;
+
+multi = n1*n2;
+
+std :: cout << "el resultado es de " << multi ;
+return 0;
+
+
+}
